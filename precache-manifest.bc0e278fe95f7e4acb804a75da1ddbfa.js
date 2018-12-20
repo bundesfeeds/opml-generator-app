50eb3eab3813641c0ec186982273068f269fd876
Sebastian Schürmann <sebs@2xs.org>
2018-12-20 17:18:23 +0100

    Updates

---
+++ precache-manifest.bc0e278fe95f7e4acb804a75da1ddbfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd459b42daceb6be6a25",
+    "url": "/opml-generator-app/static/js/runtime~main.cd459b42.js"
+  },
+  {
+    "revision": "8059b44e1a1694d714f7",
+    "url": "/opml-generator-app/static/js/main.8059b44e.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/opml-generator-app/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "8059b44e1a1694d714f7",
+    "url": "/opml-generator-app/static/css/main.81080b82.chunk.css"
+  },
+  {
+    "revision": "2742e2023a9c0100c4f72075cc60668c",
+    "url": "/opml-generator-app/index.html"
+  }
+];
